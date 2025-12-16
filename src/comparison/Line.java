be36175c657092baef9d4a5c4808aeb6efe52b73
XEROX112 <jayabhardwaz1@gmail.com>
2025-12-16 10:52:09 +0530

    Add UC2 line equality check

--- src/comparison/Line.java
+++ src/comparison/Line.java
@@ -6,7 +6,13 @@ public class Line {
     Line(int x1, int y1, int x2, int y2) {
         this.length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Line other = (Line) obj;
+        return Double.compare(this.length, other.length) == 0;
+    }
     double getLength() {
         return length;
     }
--- src/comparison/Main.java
+++ src/comparison/Main.java
@@ -3,7 +3,8 @@ package comparison;
 public class Main {
     public static void main(String[] args) {
             System.out.println("Welcome to Line Comparison Computation Program");
-            Line line = new Line(1, 2, 3, 4);
-            System.out.println(line.getLength());
+            Line line1 = new Line(1, 2, 3, 4);
+            Line line2 = new Line(1, 2, 3, 4);
+            System.out.println(line1.equals(line2));
         }
     }
